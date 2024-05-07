a8c9b54c7a46c06becc228b03d2417bb34688472
Javier Pardo <javier@amarillolimon.net>
2024-05-07 12:16:37 +0200

    remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
-import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: cloudflare(),
 })

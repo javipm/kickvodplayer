a72b33d6d2f5db75bf0bf775519a87f2d3056649
Javier Pardo <javier@amarillolimon.net>
2024-05-09 12:42:38 +0200

    fix time

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -114,6 +114,7 @@ export default function Videos({
   }
 
   const secondsToHms = (d: number) => {
+    d = Number(d) / 1000
     const h = Math.floor(d / 3600)
     const m = Math.floor((d % 3600) / 60)
     const s = Math.floor((d % 3600) % 60)
